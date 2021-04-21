348d4da5815d56076a3d5d90ae84097db47e58db
williamwjd <william.jose.wjd@gmail.com>
2021-04-21 17:27:48 -0300

    finalizando a tela welcome

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
 
 import Welcome from './src/pages/welcome';
 
 export default function App(){
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  
   return(
     <>
       <StatusBar backgroundColor="transparent" barStyle="dark-content"/>
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,50 +1,79 @@
-import React, {useState} from 'react';
-import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { Text, View, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 
-export function Welcome(){
-    return(
+export function Welcome() {
+    return (
         <View style={styles.container}>
-            <Text style={styles.title}>Gerencie {'\n'} suas plantas {'\n'} de forma fácil</Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>Gerencie {'\n'} suas plantas de {'\n'} forma fácil</Text>
+                <Image
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+                <Text style={styles.subTitle}>Não esqueça mais de regas duas plantas. Nós cuidamos de lembrar você sempre que precisar</Text>
 
-            <Image source={wateringImg} style={styles.image}/>
-
-            <Text style={styles.subTitle}>Não esqueça mais de regas duas plantas. Nós cuidamos de lembrar você sempre que precisar</Text>
-        
-            <Button />
+                <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+                    <Feather
+                        name="chevron-right"
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        flex: 1,
+    },
+
+    wrapper:{
         flex:1,
-        alignItems:'center',
-        justifyContent:'space-between'
+        alignItems: 'center',
+        justifyContent: 'space-around'
     },
 
-    title:{
-        fontSize:32,
-        fontWeight:'bold',
-        textAlign:'center',
-        color:colors.heading,
-        marginTop:38
+    title: {
+        fontSize: 28,
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38,
+        fontFamily:fonts.heading,
+        lineHeight:34
     },
 
-    image:{
-        width:292,
-        height:284
+    image: {
+        height: Dimensions.get('window').width * 0.7,
     },
 
-    subTitle:{
-        textAlign:'center',
-        fontSize:18,
-        paddingHorizontal:20,
+    subTitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
         color: colors.heading
+    },
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white,
     }
 })
 
